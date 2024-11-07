b06a3599eef425a3dcf1fdbfc8c07484f096b684
sungoh <sungoh@gmail.com>
2024-11-07 17:33:15 +0900

    chatbot_SO

--- src/layouts/BaseLayout.tsx
+++ src/layouts/BaseLayout.tsx
@@ -1,11 +1,15 @@
 import HeaderLayout from "./HeaderLayout.tsx";
+import SampleChatUI from "../components/chatbot/SampleChatUI.tsx";
+
 
 
 function BaseLayout() {
     return (
         <div className="min-h-screen bg-white">
             <HeaderLayout />
+            <SampleChatUI />
         </div>
+
 
     )
 
