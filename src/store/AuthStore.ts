c63d32c1d6bcfe920d3315bcfc64faf329aec499
ssinyong <bsy2889@gmail.com>
2024-11-13 17:18:35 +0900

    로그인 시 이름 변경 완료2_SY

--- src/store/AuthStore.ts
+++ src/store/AuthStore.ts
@@ -11,18 +11,6 @@ interface AuthState {
 const useAuthStore = create<AuthState>(
         (set) => ({
 
-            // name: '비회원',
-            // email:'',
-            // accessToken: '',
-            // setUser: (name, email, accessToken) => set(() => {
-            //
-            //     console.log("=========================================1")
-            //     console.log(name,email,accessToken)
-            //
-            //     // return {name: name, email:email, accessToken:accessToken}
-            //     return {name, email, accessToken}
-            // })
-
             name: sessionStorage.getItem('name') || '비회원', // 세션 스토리지에서 name 가져오기
             email: sessionStorage.getItem('email') || '',
             accessToken: sessionStorage.getItem('accessToken') || '',
