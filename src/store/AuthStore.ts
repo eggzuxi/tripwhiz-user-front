0b6c55cc9687a390c0b1e2c9f69b286c70268e13
eggzuxi <eggzuxi99@gmail.com>
2024-11-13 17:47:01 +0900

    forPull_JH

---
+++ src/store/AuthStore.ts
@@ -0,0 +1,33 @@
+import {create} from "zustand";
+
+
+interface AuthState {
+    name: string | null;
+    email: string | null;
+    accessToken: string | null;
+    setUser: (name: string, email: string, accessToken: string) => void;
+}
+
+const useAuthStore = create<AuthState>(
+        (set) => ({
+
+            name: sessionStorage.getItem('name') || '비회원', // 세션 스토리지에서 name 가져오기
+            email: sessionStorage.getItem('email') || '',
+            accessToken: sessionStorage.getItem('accessToken') || '',
+            setUser: (name, email, accessToken) => {
+                // 세션 스토리지에 상태 저장
+                sessionStorage.setItem('name', name);
+                sessionStorage.setItem('email', email);
+                sessionStorage.setItem('accessToken', accessToken);
+
+                console.log("=============================1")
+                console.log(name, email, accessToken)
+
+                set(() => {
+                    return { name, email, accessToken };
+                });
+            }
+        }),
+)
+
+export default useAuthStore;
