087eef0b379af95052dec28e83879d646ddf55a2
eggzuxi <eggzuxi99@gmail.com>
2024-11-25 17:35:06 +0900

    forPull_JH

--- src/api/cartAPI.ts
+++ src/api/cartAPI.ts
@@ -1,12 +1,13 @@
 import axios from "axios";
 
-// const host = 'http://localhost:8080/api/cart'
-const host = 'http://10.10.10.73:8080/api/cart'
+const host = 'http://localhost:8081/api/cart'
+// const host = 'http://10.10.10.73:8080/api/cart'
 
 export const getList = async () => {
 
     const res = await axios.get(`${host}/list`)
 
+    console.log(res.data)
     return res.data
 
 }
