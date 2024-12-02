b080b36de1cfb6ba24f2b2bca6fea02d592e5ed5
Juhui Park <129067046+eggzuxi@users.noreply.github.com>
2024-12-02 11:08:51 +0900

    forAws

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,8 @@ export default defineConfig({
   plugins: [react()],
   //CORS 에러 때문에 넣었는데 안 먹히는 듯 _SY
   server: {
+    host: "0.0.0.0", // 모든 네트워크 인터페이스에서 접속 허용
+    port: 5173,      // 기본 포트 (변경 가능)
     proxy: {
       '/api': {
         target: 'http://localhost:8081', //백엔드 서버 주소
