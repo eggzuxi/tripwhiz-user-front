b49e544b8dc556de474a99e640d137f1357b0053
eggzuxi <eggzuxi99@gmail.com>
2024-12-10 01:27:08 +0900

    SA'sCode_JH

---
+++ src/pages/order/BookingDatePage.tsx
@@ -0,0 +1,16 @@
+import CustomDatePicker from "../../components/order/CustomDatePicker";
+
+
+function BookingDatePage() {
+
+
+
+
+    return (
+        <div>
+            <CustomDatePicker/>
+        </div>
+    );
+}
+
+export default BookingDatePage;
