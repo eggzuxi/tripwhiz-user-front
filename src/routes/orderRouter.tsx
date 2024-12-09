dd586353044c6f1f1170d56e864670c4e3a48ac5
eggzuxi <eggzuxi99@gmail.com>
2024-12-10 01:26:15 +0900

    routeConnect(cartToOrder)_JH

--- src/routes/orderRouter.tsx
+++ src/routes/orderRouter.tsx
@@ -8,10 +8,20 @@ const Loading = <LoadingPage></LoadingPage>;
 // Order 관련 페이지 컴포넌트 로드
 const OrderList = lazy(() => import("../pages/order/OrderListPage")); // 주문 목록 페이지
 const OrderDetails = lazy(() => import("../pages/order/OrderDetailsPage")); // 주문 상세 페이지
+const Spot = lazy(() => import("../pages/map/GoogleMapsPage")); // 주문 상세 페이지
+const BookingDate = lazy(() => import("../pages/order/BookingDatePage")); // 주문 상세 페이지
 
 const orderRouter = {
     path: "/order",
     children: [
+        {
+            path: "spot",
+            element: <Suspense fallback={Loading}><Spot/></Suspense>
+        },
+        {
+            path: "pickup",
+            element: <Suspense fallback={Loading}><BookingDate/></Suspense>
+        },
         {
             path: "list",
             element: <Suspense fallback={Loading}><OrderList /></Suspense>, // 주문 목록 페이지
