aca5a44e9886d591e0ebef03f4a490a0bde0c81f
sungoh <sungoh@gmail.com>
2024-12-20 15:26:04 +0900

    챗봇

--- src/components/order/OrderInfo.tsx
+++ src/components/order/OrderInfo.tsx
@@ -55,7 +55,7 @@ const OrderInfo: React.FC = () => {
             });
 
             alert("결제가 완료되었습니다!");
-            navigate("/side/myorder");
+            navigate("/payment");
         } catch (error) {
             console.error("결제 중 오류 발생:", error);
             alert("결제에 실패했습니다. 다시 시도해 주세요.");
